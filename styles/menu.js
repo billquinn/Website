b4f4d5266b54ed4e4c7d55c04e1a24ffa57f2725
Bill Quinn <quinn.wi@husky.neu.edu>
2019-11-15 22:23:08 -0500

    First website commit

---
+++ styles/menu.js
@@ -0,0 +1,14 @@
+document.write('\
+\
+<link rel="stylesheet" href="menu.css"></link>\
+\
+\<ul class="topnav">\
+\
+<li class="logo">William Reed Quinn</li>\
+\
+<li><a href="../index.html">Home</a></li>\
+<li><a href="../about/index.html">About Me</a></li>\
+<li><a href="../cv/quinn-cv.pdf">CV</a></li>\
+\
+</ul>\
+')
